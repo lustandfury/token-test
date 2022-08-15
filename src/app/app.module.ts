497db75c031768a1ab39e80034b29788af5116d5
Jen <6406037+usrrname@users.noreply.github.com>
2022-08-15 17:50:40 -0400

    feat: update on theming dark and light properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { DividerComponent } from './components/divider/divider.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonComponent, DividerComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'tiaa-button',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './button.component.html',
   styleUrls: ['../../../scss/custom-theme.scss', './button.component.scss'],
 })
@@ -14,18 +16,21 @@ export class ButtonComponent {
    * Button Sizes
    */
   @Input() size: 'small' | 'medium' | 'large' = 'medium';
-
+  @Input() label = 'Button';
   /**
    * Button contents
    * @required
    */
-  @Input() label = 'Button';
+  @Input() isIconButton: boolean = false;
+  @Input() isRoundButton: boolean = false;
+  @Input() isDisabled: boolean = false;
+  @Input() color: ThemePalette;
+  @Input() icon: string = '';
+  @Input() variant: 'basic' | 'flat' | 'stroke' | 'text' | 'raised' = 'basic';
 
   @Output() onClick = new EventEmitter<Event>();
 
-  /* Properties */
-  @Input() isDisabled: boolean = false;
-  @Input() palette: 'primary' | 'accent' | 'warn' = 'primary';
-  @Input() icon: string = '';
-  @Input() variant: 'flat' | 'stroke' | 'text' = 'flat';
+  public get classes(): string[] {
+    return [`tiaa-button--${this.size}`];
+  }
 }
